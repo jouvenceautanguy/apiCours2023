05c075ea83860f75cabfb96f82d453ad4c9b83c2
iharoun <islem.haroun@my-digital-school.org>
2023-01-25 14:59:02 +0100

    app.js setting serve with express

--- src/app.js
+++ src/app.js
@@ -1,28 +1,44 @@
-const http = require('http');
+const express = require("express");
+
 const hostname = "0.0.0.0";
 const port = 3000;
+const server = express();
 
-const server = http.createServer((req,res) => {
+server.get("/",(req,res) => {
 
-    let url = req.url;
+    res.type('html');
+    res.status(200);
+    res.end("Home");
+});
+
+server.get("/posts",(req,res) => {
+
+    res.type('html');
+    res.status(200);
+    res.end("Liste des articles");
+});
+
+// const server = http.createServer((req,res) => {
+
+//     let url = req.url;
 
 
-    switch(url) {
-        case "/":
-            res.end("On est ok");
-            break;
-            case "/posts":
-                res.statusCode= 200;
-                res.setHeader = ('Content-type',"text/html");
-                res.end("Liset des articles !");
-            break;
+//     switch(url) {
+//         case "/":
+//             res.end("On est ok");
+//             break;
+//             case "/posts":
+//                 res.statusCode= 200;
+//                 res.setHeader = ('Content-type',"text/html");
+//                 res.end("Liset des articles !");
+//             break;
 
-            default:
-                    res.statusCode = 404;
-                    res.end("Erreur");
-                    break;
-    }
-})
+//             default:
+//                     res.statusCode = 404;
+//                     res.end("Erreur");
+//                     break;
+//     }
+// })
 
 
 server.listen(port,hostname);
