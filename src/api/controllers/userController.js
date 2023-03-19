27a051bb6bf83413c8b6612e8e8e2d3c6c6f7ffa
tanguy.jouvenceau <tanguy.jouvenceau@adventori.com>
2023-03-19 21:01:00 +0100

    ajout crud users

--- src/api/controllers/userController.js
+++ src/api/controllers/userController.js
@@ -1,45 +1,46 @@
 const db = require("../../db");
-// Récupération de tous les services
-exports.getAllServices = function(req, res) {
-    db.query('SELECT * FROM services', function(error, results, fields) {
+
+// Récupération de tous les utilisateurs
+exports.getAllUsers = function(req, res) {
+    db.query('SELECT * FROM users', function(error, results, fields) {
         if (error) throw error;
         res.json(results);
     });
 };
 
-// Récupération d'un service par son ID
-exports.getServiceById = function(req, res) {
-    const serviceId = req.params.id;
-    db.query('SELECT * FROM services WHERE id = ?', [serviceId], function(error, results, fields) {
+// Récupération d'un utilisateur par son ID
+exports.getUserById = function(req, res) {
+    const userId = req.params.id;
+    db.query('SELECT * FROM users WHERE id = ?', [userId], function(error, results, fields) {
         if (error) throw error;
         res.json(results[0]);
     });
 };
 
-// Création d'un nouveau service
-exports.createService = function(req, res) {
-    const { shiftType, shiftClosed } = req.body;
-    db.query('INSERT INTO services (shiftType, shiftClosed) VALUES (?, ?)', [shiftType, shiftClosed], function(error, results, fields) {
+// Création d'un nouvel utilisateur
+exports.createUser = function(req, res) {
+    const { firstname, lastname, status, active } = req.body;
+    db.query('INSERT INTO users (firstname, lastname, status, active) VALUES (?, ?, ?, ?)', [firstname, lastname, status, active], function(error, results, fields) {
         if (error) throw error;
         res.json({ id: results.insertId });
     });
 };
 
-// Mise à jour d'un service par son ID
-exports.updateService = function(req, res) {
-    const serviceId = req.params.id;
-    const { shiftType, shiftClosed } = req.body;
-    db.query('UPDATE services SET shiftType = ?, shiftClosed = ? WHERE id = ?', [shiftType, shiftClosed, serviceId], function(error, results, fields) {
+// Mise à jour d'un utilisateur par son ID
+exports.updateUser = function(req, res) {
+    const userId = req.params.id;
+    const { firstname, lastname, status, active } = req.body;
+    db.query('UPDATE users SET firstname = ?, lastname = ?, status = ?, active = ? WHERE id = ?', [firstname, lastname, status, active, userId], function(error, results, fields) {
         if (error) throw error;
-        res.json({ message: 'Service updated successfully.' });
+        res.json({ message: 'User updated successfully.' });
     });
 };
 
-// Suppression d'un service par son ID
-exports.deleteService = function(req, res) {
-    const serviceId = req.params.id;
-    db.query('DELETE FROM services WHERE id = ?', [serviceId], function(error, results, fields) {
+// Suppression d'un utilisateur par son ID
+exports.deleteUser = function(req, res) {
+    const userId = req.params.id;
+    db.query('DELETE FROM users WHERE id = ?', [userId], function(error, results, fields) {
         if (error) throw error;
-        res.json({ message: 'Service deleted successfully.' });
+        res.json({ message: 'User deleted successfully.' });
     });
 };
