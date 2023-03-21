eb657001e8c893cef9c835711412b5c58803611a
jouvenceautanguy <tanguy.jouvenceau@my-digital-school.org>
2023-03-21 18:35:34 +0100

    debut jwt et correction

--- src/api/midllewares/jwtMidlleware.js
+++ src/api/midllewares/jwtMidlleware.js
@@ -1,21 +1,21 @@
-/* const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const jwtKey = process.env.JWT_KEY;
 
-exports.verifyToken = (req, res) => {
+exports.verifyToken = (req, res, next) => {
   let token = req.headers["authorization"];
   if (token !== undefined) {
     jwt.verify(token, jwtKey, (error, payload) => {
       if (error) {
         console.log(error);
         res.status(403);
-        res.json({ message: "Acces interdit : token manquant" });
+        res.json({ message: "Access denied: Invalid token" });
       } else {
+        req.user = payload.user;
         next();
       }
     });
   } else {
     res.status(403);
-    res.json({ message: "Acces interdit " });
+    res.json({ message: "Access denied: Token missing" });
   }
 };
- */
--- src/api/routes/paymentRoute.js
+++ src/api/routes/paymentRoute.js
@@ -1,17 +1,18 @@
 module.exports = (server) => {
     const paymentController = require('../controllers/paymentController');
+    const middleware = require("../midllewares/jwtMidlleware");
 
     // Récupération de tous les paiements
-    server.get('/payments', paymentController.getAllPayments);
+    server.get('/payments', middleware.verifyToken, paymentController.getAllPayments);
 
     // Récupération du solde par l'id_user
-    server.get('/payments/user/:id', paymentController.getPaymentByUserId);
+    server.get('/payments/user/:id', middleware.verifyToken, paymentController.getPaymentByUserId);
 
 // récupérer tous les soldes de tous les Users. 
-    server.get('payments/allUser', paymentController.getAllPaymentsAllUsers);
+    server.get('payments/allUser', middleware.verifyToken, paymentController.getAllPaymentsAllUsers);
 
     // Création d'un nouveau paiement
-    server.post('/payments', paymentController.createPayment);
+    server.post('/payments', middleware.verifyToken, paymentController.createPayment);
 
     // Suppression d'un paiement par son ID
     server.delete('/payments/:id', paymentController.deletePayment);
