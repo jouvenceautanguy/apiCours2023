5da633e79fe495b94ad0c1b7909f0c7beb7c6ff7
jouvenceautanguy <tanguy.jouvenceau@my-digital-school.org>
2023-03-21 11:57:47 +0100

    Ajout CrudTips & fonctionnalités requises pour front

--- src/api/controllers/tipsController.js
+++ src/api/controllers/tipsController.js
@@ -2,7 +2,7 @@ const db = require('../../db');
 
 // Récupération de tous les tips
 exports.getAllTips = function(req, res) {
-  db.query('SELECT * FROM tableTips', function(error, results, fields) {
+  db.query('SELECT * FROM tableTips', function(error, results) {
     if (error) throw error;
     res.json(results);
   });
@@ -11,16 +11,16 @@ exports.getAllTips = function(req, res) {
 // Récupération d'un tip par son ID
 exports.getTipById = function(req, res) {
   const tipId = req.params.id;
-  db.query('SELECT * FROM tableTips WHERE id = ?', [tipId], function(error, results, fields) {
+  db.query('SELECT * FROM tableTips WHERE id = ?', [tipId], function(error, results) {
     if (error) throw error;
     res.json(results[0]);
   });
 };
 
-// Création d'un nouveau tip
+// Création d'un nouveau tip // variable dans front pour remplacer le ID Service
 exports.createTip = function(req, res) {
   const { tips, id_restaurantTable, id_service } = req.body;
-  db.query('INSERT INTO tableTips (tips, id_restaurantTable, id_service) VALUES (?, ?, ?)', [tips, id_restaurantTable, id_service], function(error, results, fields) {
+  db.query('INSERT INTO tableTips (tips, id_restaurantTable, id_service) VALUES (?, ?, ?)', [tips, id_restaurantTable, id_service], function(error, results) {
     if (error) throw error;
     res.json({ id: results.insertId });
   });
@@ -28,9 +28,8 @@ exports.createTip = function(req, res) {
 
 // Mise à jour d'un tip par son ID
 exports.updateTip = function(req, res) {
-  const tipId = req.params.id;
-  const { tips, id_restaurantTable, id_service } = req.body;
-  db.query('UPDATE tableTips SET tips = ?, id_restaurantTable = ?, id_service = ? WHERE id = ?', [tips, id_restaurantTable, id_service, tipId], function(error, results, fields) {
+  const { tips, id_restaurantTable, tipId} = req.body;
+  db.query('UPDATE tableTips SET tips = ?, id_restaurantTable = ? WHERE id = ?', [tips, id_restaurantTable,  tipId], function(error) {
     if (error) throw error;
     res.json({ message: 'Tip updated successfully.' });
   });
@@ -39,8 +38,16 @@ exports.updateTip = function(req, res) {
 // Suppression d'un tip par son ID
 exports.deleteTip = function(req, res) {
   const tipId = req.params.id;
-  db.query('DELETE FROM tableTips WHERE id = ?', [tipId], function(error, results, fields) {
+  db.query('DELETE FROM tableTips WHERE id = ?', [tipId], function(error) {
     if (error) throw error;
     res.json({ message: 'Tip deleted successfully.' });
   });
 };
+
+// get tous les tips du service actuel. select * from tabletips where shiftclosed = 0 en affichant aussi la donnée shifttype
+exports.getTipServiceUp = function(req, res) {
+ db.query('SELECT *  FROM tableTips INNER JOIN services ON tableTips.id_service = services.id WHERE services.shiftClosed = 0', function(error, results){
+  if (error) throw error;
+  res.json(results);
+ }) 
+}
--- src/api/routes/tipsRoute.js
+++ src/api/routes/tipsRoute.js
@@ -1,19 +1,20 @@
 module.exports = (server) => {
-    const tipsController = require('../controllers/tipsController');
-    
-    // Récupération de tous les services
-    server.get('/tips', tipsController.getAllTips);
-    
-    // Récupération d'un service par son ID
-    server.get('/tips/:id', tipsController.getTipById);
-    
-    // Création d'un nouveau service
-    server.post('/tips', tipsController.createTip);
-    
-    // Mise à jour d'un service par son ID
-    server.put('/tips/:id', tipsController.updateTip);
-    
-    // Suppression d'un service par son ID
-    server.delete('/tips/:id', tipsController.deleteTip);
-    
-    };
+  const tipsController = require("../controllers/tipsController");
+  // Récupération de tous les tips
+  server.get("/tips", tipsController.getAllTips);
+
+  // récupère les tips du service actuel pour la vue
+  server.get("/tips/tipService", tipsController.getTipServiceUp);
+  
+  // modif d'un tip par son ID
+  server.get("/tips/:id", tipsController.getTipById);
+
+  // Création d'un nouveau tip
+  server.post("/tips", tipsController.createTip);
+
+  // Mise à jour d'un tip par son ID
+  server.put("/tips/:id", tipsController.updateTip);
+
+  // Suppression d'un tip par son ID
+  server.delete("/tips/:id", tipsController.deleteTip);
+};
