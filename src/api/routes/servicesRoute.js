cb6dc796b831d7d4741dfb661d51c0f9205b29c6
jouvenceautanguy <tanguy.jouvenceau@my-digital-school.org>
2023-03-22 12:37:52 +0100

    ajout collection et license

--- src/api/routes/servicesRoute.js
+++ src/api/routes/servicesRoute.js
@@ -1,11 +1,17 @@
 module.exports = (server) => {
 const serviceController = require('../controllers/serviceController');
 
+//récupération du service actuellement UP
+server.get('/services/up', serviceController.getServiceUp);
+//récupération des utilisateurs en service
+server.get('/services/users', serviceController.getServiceUsers);
 // Récupération de tous les services
 server.get('/services', serviceController.getAllServices);
 
 // Récupération d'un service par son ID
 server.get('/services/:id', serviceController.getServiceById);
+
+
 
 // Création d'un nouveau service
 server.post('/services', serviceController.createService);
@@ -16,9 +22,7 @@ server.put('/services/:id', serviceController.updateService);
 // Suppression d'un service par son ID
 server.delete('/services/:id', serviceController.deleteService);
 
-//récupération des utilisateurs en service
-server.get('/services/users', serviceController.getServiceUsers);
 
-//récupération du service actuellement UP
-server.get('/services/up', serviceController.getServiceUp);
+
+
 };
